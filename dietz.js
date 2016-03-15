fb050d304358a396223cfbd3d0f467988fb6b77b
Johanna <johanna.dietzz@gmail.com>
2016-03-15 21:07:58 +0100

    template aktualisiert

--- dietz.js
+++ dietz.js
@@ -1,36 +1,47 @@
-
-
-//items = new Mongo.Collection("items");
+var Items = new Mongo.Collection("items");
 
 
 if (Meteor.isClient) {
-  /*// This code only runs on the client
-  Template.body.helpers({
-    items: function () {
-      return items.find({});
-    }
-  });
-}
-*/
+    // This code only runs on the client
+    Template.body.helpers({
+        items: function() {
+            return [{
+                title: 'Bauernbrot',
+                price: '3,50',
+                image: 'http://www.baecker-baier.de/wp-content/uploads/2014/05/1315-SaatenQuark.jpg',
+                ingredients1: '70% Roggenmehl Type 1150',
+                ingredients2: '30% Weizenmehl Type 1050',
+                Triebmittel: 'Sauerteig',
+            }, {
+                title: 'Dinkelbrot',
+                price: '4,50',
+                image: 'http://www.baecker-baier.de/wp-content/uploads/2010/03/12-DinkelFerment-1.jpg',
+                ingredients1: '100% Dinkelmehl Type 1050',
+                Triebmittel: 'Sauerteig'
+            }]; //Items.find({});
+        }
+    });
 
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
+
+
+    Template.hello.helpers({
+        counter: function() {
+            return Session.get('counter');
+        }
+    });
+
+    Template.hello.events({
+        'click button': function() {
+            // increment the counter when button is clicked
+            Session.set('counter', Session.get('counter') + 1);
+        }
+    });
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+    Meteor.startup(function() {
+        // code to run on server at startup
+    });
 }
-
